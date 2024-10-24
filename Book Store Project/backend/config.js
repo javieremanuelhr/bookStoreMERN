bbc57b28c9c5533c3efb6a3a236ce012d6ed4beb
javieremanuelhr <javieremanuelhr@gmail.com>
2024-10-24 05:25:13 -0600

    checkig if 2nd commit is working or not

--- Book Store Project/backend/config.js
+++ Book Store Project/backend/config.js
@@ -1,2 +1,3 @@
 export const PORT = 5000;
 
+export const MONGODB_URL = 'mongodb+srv://javieremanuelhr:XmMoSvPRqZWngCip@bookstoremern.hpvwq.mongodb.net/?retryWrites=true&w=majority&appName=bookStoreMERN';
--- Book Store Project/backend/index.js
+++ Book Store Project/backend/index.js
@@ -1,13 +1,104 @@
 import express from 'express';
-import {PORT} from './config.js';
+import {PORT, MONGODB_URL} from './config.js';
+import mongoose from 'mongoose';
+import {Book} from './models/bookModel.js';
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     console.log(res);
     return res.status(200).send('welcome to MERN stack tutorial');
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.post('/books', async (req, res) => {
+    try {
+        if (
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear
+        ) {
+            return res.status(400).send({message: 'All fields are required'});
+        }
+
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear
+        };
+
+        const book = await Book.create(newBook);
+
+        return res.status(201).send(book);
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+});
+
+//get all books
+
+app.get('/books', async (req, res) => {
+    try {
+        const books = await Book.find();
+        return res.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+})
+
+//find book by Id
+
+app.get('/books/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const book = await Book.findById(id);
+        return res.status(200).json(book);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+})
+    
+//update a book
+
+app.put('/books/:id', async (req, res) => {
+    try {
+        if (
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear
+        ) {
+            return res.status(400).send({message: 'All fields are required'});
+        }
+
+        const {id} = req.params;
+
+        const result = await Book.findByIdAndUpdate(id, req.body);
+
+        if (!result) {
+            return res.status(404).json({message: 'Book not found'});
+        }
+        
+        return res.status(200).send({ message: 'Book updated successfully' });
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+});
+
+mongoose.connect(MONGODB_URL).then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch((error) => {
+    console.log(error);
 })
